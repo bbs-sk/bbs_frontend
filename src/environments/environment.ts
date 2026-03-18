f7edacd8bc679b6b8fc4bf57dc85890028640133
anvy17 <anvyluna1712@gmail.com>
2026-03-18 13:14:24 +0700

    update vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'http://localhost:3000'
+  //apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://bbs-backend-three.vercel.app/api'
 };
