e9470977f60e011a0587dc237bd3ac62b8caaf5c
bbs-sk <skripsibbs@gmail.com>
2026-06-10 22:24:01 +0700

    P

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
-  // apiUrl: 'https://bbs-backend-three.vercel.app/api'
+  production: false
+  // apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://bbs-backend-three.vercel.app/api'
 };
